7684cedbf55fdd51a208d45e12506fe17a517759
Evangelina Garcia <evangelinagarcia3@hotmail.com>
2025-10-14 10:26:35 -0300

    actualizacion POO

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,40 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
-    ArrayList<String> productosDB = ProductosDecoracion();
-    //eleccion de funcionalidad
+    ArrayList<Producto> productosDB = obtenerProductosDecoracion();
+    int idSiguiente = productosDB.size() + 1;
     int opcionUsuario;
-    System.out.println("Bienvenido a tu ecommerce ");
+
+    System.out.println("Bienvenidos a la app de compras 🛒");
     label:
-
-
     while (true) {
       System.out.println("""
-        
-        Ingrese la opcion de desea realizar:
-         0 - Finaliza el programa
-         1 - Crea un producto
-         2 - Listar productos
-         3 - Busqueda por nombre
-         4 - Editar nombre producto
-         5 - Borrar productos
-         """);
+          Eliga la opción deseada ingresando el número:
+          0 - Finaliza el programa
+          1 - Crea un Producto
+          2 - Listar Productos
+          3 - Búsqueda por nombre
+          4 - Editar nombre producto
+          5 - Borrar producto
+          6 - nueva funcion
+          """);
       opcionUsuario = entrada.nextInt();
+
       switch (opcionUsuario) {
-        case 1 -> crearArticulo(productosDB); // - >
-        case 2 -> listarArticulos(productosDB);
-        case 3 -> buscarArticuloPorNombre(productosDB);
+        case 1 -> {
+          crearProducto (idSiguiente, productosDB);
+          idSiguiente += 1;
+        }
+        case 2 -> listarProductos(productosDB);
+        case 3 -> buscarProductoPorNombre(productosDB);
+        case 4 -> editarProducto(productosDB);
+        case 5 -> borrarProducto(productosDB);
+        case 6 -> System.out.println("Funcionalida en progreso...");
         case 0 -> {
           System.out.println("Gracias por usar la app!");
           break label; // corta el bucle donde se ejecuta
@@ -38,48 +43,104 @@ public class Main {
       }
     }
   }
-  public static void crearArticulo(ArrayList<String> productos) {
+
+  public static void crearProducto (int id, ArrayList<Producto> productos) {
     Scanner entrada = new Scanner(System.in);
-    System.out.println("Creando nuevo Artículo");
+    System.out.println("Creando Nuevo Producto");
     System.out.print("Ingrese el nombre del nuevo producto: ");
     String nombre = entrada.nextLine();
-    System.out.println("se creo Articulo " + nombre);
 
-    productos.add(nombre);
+    productos.add(new Producto(id, nombre));
+    System.out.print("El articulo ingresado es "+ nombre );
+
+    //
     pausa();
   }
-  public static void listarArticulos(ArrayList<String> articulos) {
-    System.out.println("#######################################");
-    System.out.println("        LISTA DE PRODUCTOS");
-    System.out.println("#######################################");
 
-    if (articulos == null || articulos.isEmpty()) {
-      System.out.println("⚠️  No hay articulos para mostrar.");
+  public static void listarProductos(ArrayList<Producto> productos) {
+    System.out.println("=======================================");
+    System.out.println("        LISTA DE PRODUCTOS");
+    System.out.println("=======================================");
+
+    if (productos == null || productos.isEmpty()) {
+      System.out.println("⚠️  No hay productos para mostrar.");
     } else {
       int contador = 1;
-      for (String articulo : articulos) {
-
-        System.out.printf(" %2d. %s%n", contador++, articulo);
+      for (Producto producto : productos) {
+        System.out.printf(" %2d. %s%n", producto.id, producto.nombre);
       }
-
     }
 
     System.out.println("=======================================");
     pausa();
   }
-  public static void buscarArticuloPorNombre(ArrayList<String> articulos) {
+
+  public static void buscarProductoPorNombre(ArrayList<Producto> productos) {
     Scanner entrada = new Scanner(System.in);
     System.out.println("Ingrese un nombre de un producto: ");
     String busqueda = entrada.nextLine();
-    ArrayList<String> articuloEncontrados = new ArrayList<>();
+    ArrayList<Producto> productoEncontrados = new ArrayList<>();
 
-    for (String articulo : articulos) {
-      if (estaIncluido(articulo, busqueda)) {
-        articuloEncontrados.add(articulo);
+    for (Producto producto : productos) {
+      if (estaIncluido(producto.nombre, busqueda)) {
+        productoEncontrados.add(producto);
       }
     }
 
-    listarArticulos(articuloEncontrados);
+    listarProductos(productoEncontrados);
+  }
+
+  public static void editarProducto(List<Producto> productos) {
+    System.out.println("ingrese id del producto a editar");
+    Scanner entrada = new Scanner(System.in);
+    Producto producto = obtenerProductoPorId(productos);
+
+    // TODO: validar que encontramos el indice
+    String nombreOriginal = producto.nombre;
+    System.out.println("Producto a editar:");
+    System.out.println(nombreOriginal);
+    // TODO: validar que el usuario quiere editar el producto que se encontro
+    System.out.print("Ingrese el nuevo nombre: ");
+    String nuevoNombre = entrada.nextLine();
+
+
+    producto.nombre = nuevoNombre;
+
+    System.out.printf("El nombre del producto cambio de %s a %s", nombreOriginal, nuevoNombre);
+  }
+
+  public static void borrarProducto(List<Producto> productos) {
+    System.out.println("ingrese id del producto a borrar");
+    Scanner entrada = new Scanner(System.in);
+    Producto producto = obtenerProductoPorId(productos);
+
+    // TODO: validar que encontramos el indice
+    String nombreOriginal = producto.nombre;
+    System.out.println("Producto a eliminar:");
+    System.out.println(nombreOriginal);
+    // TODO: validar que el usuario quiere borrar el producto que se encontro
+
+    productos.remove(producto);
+
+    System.out.printf("El producto %s se borro", nombreOriginal);
+
+    pausa();
+  }
+
+  /* UTILIDADES */
+  /* Busqueda por id - ahora mismo solo funciona con el indice, en el futuro se va a cambiar */
+  public static Producto obtenerProductoPorId(List<Producto> productos) {
+    Scanner entrada = new Scanner(System.in);
+    // TODO: validacion de datos
+    System.out.println("Ingrese el id del producto: ");
+    int idBusqueda = entrada.nextInt();
+
+    for (Producto producto : productos) {
+      if (producto.id == idBusqueda) {
+        return producto;
+      }
+    }
+    return null; // el null representa que no encontramos el producto
   }
   public static boolean estaIncluido(String nombreCompleto, String nombreParcial) {
     String nombreCompletoFormateado = formatoBusqueda(nombreCompleto);
@@ -90,6 +151,7 @@ public class Main {
   public static String formatoBusqueda(String texto) {
     return texto.trim().toLowerCase();
   }
+
   public static void pausa() {
     Scanner entrada = new Scanner(System.in);
     System.out.println("Pulse ENTER para continuar...");
@@ -97,24 +159,24 @@ public class Main {
     for (int i = 0; i < 20; ++i) {
       System.out.println();
     }
+    // TODO: limpiar la pantalla de la consola
   }
-  public static ArrayList<String> ProductosDecoracion() {
-    ArrayList<String> productos = new ArrayList<>();
 
-    productos.add("Mesa TV+ Escritorio con tapa en madera clara");
-    productos.add("Comoda Vajillero modelo Pampa");
-    productos.add("Panel flotante de TV revistido en madera");
-    productos.add("Biblioteca Modelo kenia de melamina 18MM");
-    productos.add("Placard Nordico MODELO MONACO Escandinavo Paraiso Laqueado Puertas Melamina blanca");
-    productos.add("Biblioteca Paraiso Laqueado y Melamina Modelo Montevideo");
-    productos.add("lampara de pie, negra articulada");
-    productos.add("juego de mesa con 4 sillas estilo Nordico");
-    productos.add("sillon 2 cuerpos gamuza");
-    productos.add("ALACENA superior JUANA puertas revatibles");
-    productos.add("Cama Rusticda de cedro moderna matrimonial");
+  public static ArrayList<Producto> obtenerProductosDecoracion() {
+    ArrayList<Producto> productos = new ArrayList<>();
 
+    productos.add(new Producto(1,"Mesa TV+ Escritorio con tapa en madera clara"));
+    productos.add(new Producto(2,"Comoda Vajillero modelo Pampa"));
+    productos.add(new Producto(3,"Panel flotante de TV revistido en madera"));
+    productos.add(new Producto(4,"Biblioteca Modelo kenia de melamina 18MM"));
+    productos.add(new Producto(5,"Placard Nordico MODELO MONACO Escandinavo Paraiso Laqueado Puertas Melamina blanca"));
+    productos.add(new Producto(6,"Biblioteca Paraiso Laqueado y Melamina Modelo Montevideo"));
+    productos.add(new Producto(7,"lampara de pie, negra articulada"));
+    productos.add(new Producto(8,"juego de mesa con 4 sillas estilo Nordico"));
+    productos.add(new Producto(9,"sillon 2 cuerpos gamuza "));
+    productos.add(new Producto(10,"ALACENA superior JUANA puertas revatibles"));
+    productos.add(new Producto(11,"Cama Rusticda de cedro moderna matrimonial"));
 
     return productos;
   }
 }
-
---
+++ src/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+
+  int id;
+  String nombre;
+
+  // metodo constructor
+  public Producto(int idProducto, String nombreProducto) {
+    id = idProducto;
+    nombre = nombreProducto;
+  }
+}
